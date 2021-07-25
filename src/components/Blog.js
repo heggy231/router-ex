8fa2ec91bd67e095d6ad7923a2072a965a06ec94
Heggy Castaneda <heggyy@gmail.com>
2021-07-25 11:56:56 -0400

    setup map

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import {
+  Link
+} from 'react-router-dom';
+
 const blogPosts = [
   {
     title: "Blog post #1",
@@ -17,32 +21,24 @@ const blogPosts = [
   },
 ];
 
-const Blog = (props) => (
-  <>
-    <h1>Crypto Currency</h1>
-    <article>
-      <h2>Blog post #1</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </article>
-    <article>
-      <h2>Blog post #2</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro
-        iusto placeat error assumenda veniam ad soluta velit quam esse explicabo
-        dolorum pariatur suscipit natus, accusamus saepe laboriosam architecto
-        repellat!
-      </p>
-    </article>
-    <article>
-      <h2>Blog post #3</h2>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius qui
-        nesciunt itaque nostrum incidunt, temporibus molestias? Non vel
-        aspernatur, deserunt alias, nemo esse accusamus tempora recusandae
-        praesentium repellat illo ipsum?
-      </p>
-    </article>
-  </>
-);
+const Blog = (props) => {
+  return (
+    <>
+      <h1>Blog about Crypto Currency</h1>
+      <nav>
+        {
+          blogPosts.map((post, index) => (
+            <>
+              <Link to={`/blog/${index}`}>
+                {post.title}
+              </Link>
+              <br />
+            </>
+          ))
+        }
+      </nav>
+    </>
+  );
+};
 
 export default Blog;
