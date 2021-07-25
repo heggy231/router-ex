e68d725e2e9be605672d8181897859b201f4e523
Heggy Castaneda <heggyy@gmail.com>
2021-07-25 10:39:47 -0400

    done setup pages

---
+++ src/components/Blog.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default (props) => (
+  <>
+    <h1>Crypto Currency</h1>
+    <article>
+      <h2>Blog post #1</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+    </article>
+    <article>
+      <h2>Blog post #2</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi porro iusto placeat error assumenda veniam ad soluta velit quam esse explicabo dolorum pariatur suscipit natus, accusamus saepe laboriosam architecto repellat!</p>
+    </article>
+    <article>
+      <h2>Blog post #3</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius qui nesciunt itaque nostrum incidunt, temporibus molestias? Non vel aspernatur, deserunt alias, nemo esse accusamus tempora recusandae praesentium repellat illo ipsum?
+      </p>
+    </article>
+  </>
+)
